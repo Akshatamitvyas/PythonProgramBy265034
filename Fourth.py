375a276c1c5d247c36fc49203413775f5eea774c
Akshat Vyas <akshat.amitvyas@gmail.com>
2021-04-20 09:38:56 +0530

    Today's task

---
+++ Fourth.py
@@ -0,0 +1,7 @@
+n=float(input())
+if(n>=90 and n<=100):
+    print("You got distinction")
+elif(n>=60 and n<90):
+    print("You have Passed the examination")
+else:
+    print("Failed or Try again")
