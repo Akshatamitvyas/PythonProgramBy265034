ba6231730bd4efed7256fd5b8c885d31e83f53e7
Akshatamitvyas <47518001+Akshatamitvyas@users.noreply.github.com>
2021-04-23 06:38:38 +0530

    Add files via upload

---
+++ ClassPr3.py
@@ -0,0 +1,11 @@
+s=input("Enter the abbreviation:")
+if(s=="lol"):
+    print("laughing out loud")
+elif(s=="rofl"):
+    print("rolling on the floor laughing")
+elif ( s == "lmk"):
+    print("let me know")
+elif (s=="smh"):
+    print("shaking my head")
+else:
+    print(s)
